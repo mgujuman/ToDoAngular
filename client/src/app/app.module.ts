3f971dde0d1c60941522b6d017fd6bf7cd67de34
Michael Gujuman <mgujuman.exceedteam@gmail.com>
2019-09-12 17:28:54 +0300

    create top

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RootComponent } from './root/root.component';
+import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { ROUTING } from './app.routing';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    RootComponent,
+    AppComponent,
     LoginComponent,
+    TopBarComponent,
+    RegistrationComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -17,6 +23,6 @@ import { ROUTING } from './app.routing';
     FormsModule,
   ],
   providers: [],
-  bootstrap: [RootComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,10 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
- 
+import { HomeComponent } from './home/home.component'
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent} from './registration/registration.component'
+
  
 export const AppRoutes: Routes = [
-    { path: '', component: LoginComponent }
+    { path: '', component: HomeComponent },
+    { path: 'signIn', component: LoginComponent },
+    { path: 'signUp', component: RegistrationComponent },
+
 ];
  
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
