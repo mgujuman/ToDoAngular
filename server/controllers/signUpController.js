a46ee84e295b3ab773490b8ca836b1bc4c2339a6
Michail Gujuman <mgujuman.exceedteam@gmail.com>
2019-09-15 20:30:22 +0300

    Added check for used email

--- server/controllers/signUpController.js
+++ server/controllers/signUpController.js
@@ -4,20 +4,20 @@ exports.signUp = (req, res) =>{
   console.log('REGISTRATION successfully, email is ',req.body.email,' password is ',req.body.password);
  
   const { email, password } = req.body;
-  let flag = false;
-  console.log(flag);
   User.find({ email },(err,user) => {
     if (err) return  console.log(err);
     console.log(user.length);
     if (user.length === 0) {
-      flag = true;
-    };
+      const user = new User({ email, password });
+      user.save ((err) => { if (err) return  console.log(err)});
+      console.log("add new user");
+      return res.status(200).json({
+        status: "succes",
+      });
+    } else {
+      return res.status(200).json({
+        status: 'fail',
+      });
+    }
   });
-  console.log(flag);
-  if (flag){
-    //const user = new User({ email, password });
-    //user.save ((err) => { if (err) return  console.log(err)});
-    console.log("add new user");
-  };
-  res.send(flag);
 };
